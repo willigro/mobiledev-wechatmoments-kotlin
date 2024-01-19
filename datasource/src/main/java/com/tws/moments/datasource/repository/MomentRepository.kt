ba251e37409d43155be05c008c7b36f937de03ea
Rittmann <willirittmann@outlook.com>
2024-01-19 12:58:10 -0300

    Repository must make requests inside the IO dispatcher

--- datasource/src/main/java/com/tws/moments/datasource/repository/MomentRepository.kt
+++ datasource/src/main/java/com/tws/moments/datasource/repository/MomentRepository.kt
@@ -3,6 +3,8 @@ package com.tws.moments.datasource.repository
 import com.tws.moments.datasource.api.MomentService
 import com.tws.moments.datasource.api.entry.TweetBean
 import com.tws.moments.datasource.api.entry.UserBean
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 interface MomentRepository {
@@ -12,12 +14,13 @@ interface MomentRepository {
 
 class MomentRepositoryImpl @Inject constructor(
     private val momentService: MomentService,
-): MomentRepository {
-    override suspend fun fetchUser(): UserBean {
-        return momentService.user("jsmith")
+    // TODO (rittmann) add a cache, can be a DAO, Preferences, File, anything
+) : MomentRepository {
+    override suspend fun fetchUser(): UserBean = withContext(Dispatchers.IO) {
+        momentService.user("jsmith")
     }
 
-    override suspend fun fetchTweets(): List<TweetBean> {
-        return momentService.tweets("jsmith")
+    override suspend fun fetchTweets(): List<TweetBean> = withContext(Dispatchers.IO) {
+        momentService.tweets("jsmith")
     }
 }
