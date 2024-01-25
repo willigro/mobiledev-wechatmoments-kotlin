cd9e6f1cc52cec00730c8d3927f265593049357a
Rittmann <willirittmann@outlook.com>
2024-01-24 22:47:15 -0300

    Remove ScreenAdaptiveUtil

--- app/src/main/java/com/tws/moments/TWApplication.kt
+++ app/src/main/java/com/tws/moments/TWApplication.kt
@@ -1,13 +1,11 @@
 package com.tws.moments
 
 import android.app.Application
-import com.tws.moments.designsystem.utils.ScreenAdaptiveUtil
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class TWApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        ScreenAdaptiveUtil.adaptive(this)
     }
 }
