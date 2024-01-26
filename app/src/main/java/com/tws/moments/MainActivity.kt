3d38251f33d141d14700bcde506a5d415c072226
Rittmann <willirittmann@outlook.com>
2024-01-26 15:36:41 -0300

    Separate the statusBar from the app
    - Removing the FULLSCREEN flag

--- app/src/main/java/com/tws/moments/MainActivity.kt
+++ app/src/main/java/com/tws/moments/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.tws.moments
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.tws.moments.designsystem.theme.TwsMomentsTheme
@@ -13,18 +11,11 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initWindow()
 
         setContent {
             TwsMomentsTheme {
                 MainScreenRoot()
             }
         }
-    }
-
-    private fun initWindow() {
-        val flag = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        window.decorView.systemUiVisibility = flag
-        window.statusBarColor = Color.TRANSPARENT
     }
 }
